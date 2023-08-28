1f18a8f15152b9ed58627728c3848c76589098a2
Edmuna <ereda.hr@gmail.com>
2023-08-28 22:16:22 +0200

    inital commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const navbarUL = document.querySelector(".navbar__ul")
+const xIcon = document.querySelector(".navbar__x")
+const hamburgerIcon = document.querySelector(".hamburger__navbar")
+const firstPageContainer = document.querySelector(".container")
+
+const secondSection = document.querySelector(".second__section")
+
+const form = document.getElementById("form")
+const sender = document.getElementById("name")
+const email = document.getElementById("email")
+const message = document.getElementById("message")
+
+hamburgerIcon.addEventListener("click", () => {
+    navbarUL.classList.toggle("ul__inactive")
+    xIcon.classList.toggle("not__active")
+    hamburgerIcon.classList.toggle("not__active")
+    firstPageContainer.classList.toggle("not__active")
+    secondSection.classList.toggle("not__active")
+})
+
+
+xIcon.addEventListener("click", () => {
+    navbarUL.classList.toggle("ul__inactive")
+    xIcon.classList.toggle("not__active")
+    hamburgerIcon.classList.toggle("not__active")
+    firstPageContainer.classList.toggle("not__active")
+    secondSection.classList.toggle("not__active")
+})
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const templateParams = {
+        sender: sender.value,
+        email: email.value,
+        message: message.value,
+    };
+
+    emailjs.send('service_q83x05s', 'template_wle2anc', templateParams, "1iqi5gSawssZ1Xp36")
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text, response, templateParams);
+            sender.value = "";
+            email.value = "";
+            message.value = "";
+            // Here the content of the button should be changed or some notification that the e-mail has been succesfully sent
+        })
+        .catch(function (error) {
+            console.log('FAILED...', error);
+        });
+});
+
